e41088624f9f87f178e1be22da793b8f7222877f
Li Swafford <liswafford@yahoo.com>
2019-01-08 13:49:12 -0600

    start

--- src/App.js
+++ src/App.js
@@ -5,21 +5,21 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="game">
+        <div className="head">
+          World's best tic tac toe 
+        </div>
+        <div className="board">
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+        </div>
       </div>
     );
   }
